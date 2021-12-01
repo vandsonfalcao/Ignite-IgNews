382acc329f401e766cbc0087c3e70edb3bf361b3
Vandson <vandsonsf@gmail.com>
2021-12-01 10:54:04 -0300

    feat(home): SSR para SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from "../services/stripe";
@@ -38,10 +38,8 @@ export default function Home({ product }: HomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const price = await stripe.prices.retrieve("price_1K1faiICN214fPoT1dmaxGq3", {
-    expand: ["product"], // informaçoes do produto
-  });
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve("price_1K1faiICN214fPoT1dmaxGq3");
 
   const product = {
     priceId: price.id,
@@ -55,5 +53,26 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, // 24 hours
   };
 };
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const price = await stripe.prices.retrieve("price_1K1faiICN214fPoT1dmaxGq3", {
+//     expand: ["product"], // informaçoes do produto
+//   });
+
+//   const product = {
+//     priceId: price.id,
+//     amount: new Intl.NumberFormat("en-US", {
+//       style: "currency",
+//       currency: "USD",
+//     }).format(price.unit_amount / 100),
+//   };
+
+//   return {
+//     props: {
+//       product,
+//     },
+//   };
+// };
