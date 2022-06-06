c6b845e98f3c27dce789caa5505a859a776de0f1
Vandson Falcão <vandson.falcao@inbec.com.br>
2022-06-06 16:57:07 -0300

    add faunadb

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,11 +1,47 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import { query } from "faunadb";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
+import { fauna } from "../../../services/fauna";
 
 export default NextAuth({
-  providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
-    }),
-  ],
-})
+	providers: [
+		GithubProvider({
+			clientId: process.env.GITHUB_ID,
+			clientSecret: process.env.GITHUB_SECRET,
+			authorization: {
+				params: {
+					scope: "read:user",
+				},
+			},
+		}),
+	],
+	callbacks: {
+		async signIn({ user, account, profile }) {
+			const { name, email, id } = user;
+			try {
+				await fauna.query(
+					query.If(
+						query.Not(
+							query.Exists(
+								query.Match(
+									query.Index("user_by_email"),
+									query.Casefold(user.email)
+								)
+							)
+						),
+						query.Create(query.Collection("users"), {
+							data: { id: id, name: name, email: email },
+						}),
+						query.Get(
+							query.Match(query.Index("user_by_email"), query.Casefold(user.email))
+						)
+					)
+				);
+
+				return true;
+			} catch (error) {
+				return false;
+			}
+		},
+	},
+});
